ae5014f1f2d80c8b00339f2b21e7163846e77844
Wills Grave <will@keydigitalconsulting.com>
2019-08-19 18:17:13 -0400

    Make sure keys are being tracked.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,33 @@
 // Creates an array that lists out all of the options (Alphabet & Movie Answers).
 // var userChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; //
 
-var words = ['Rudy', 'Jurrassic Park', 'Star Wars', 'The Princess Bride', 'Lord of the Rings'];
+var word = ['Rudy', 'Jurassic Park', 'Star Wars', 'The Princess Bride', 'Lord of the Rings'];
+
 
 //Picks Movie Answer at Random//
-let choosenWord = Math.floor(Math.random() * words.length);
+let randNum = Math.floor(Math.random() * word.length);
+let choosenWord = word[randNum];
+console.log(choosenWord);
+let underScore = [];
 console.log(choosenWord);
 
+
 //Creates Underscores based off length of Movie Answer//
+let generateUnderscore = () => {
+    for (var i = 0; i < choosenWord.length; i++) {
+      underScore.push('_');
+    }
+    return underScore;
+}
+
+console.log(generateUnderscore());
+
 
 //Get Users Guess //
+document.addEventListener('keypress', (event) => {
+  console.log(event);
+});
+
 
 //Check if guess is Correct //
 
