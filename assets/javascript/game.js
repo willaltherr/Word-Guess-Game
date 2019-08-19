273f9d354d8e9a1453334a8f282cf898ad6f7209
Wills Grave <will@keydigitalconsulting.com>
2019-08-19 08:56:26 -0400

    Got the words in an array and made sure the function picks random words.

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+// Creates an array that lists out all of the options (Alphabet & Movie Answers).
+// var userChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; //
+
+var words = ['Rudy', 'Jurrassic Park', 'Star Wars', 'The Princess Bride', 'Lord of the Rings'];
+
+//Picks Movie Answer at Random//
+let choosenWord = Math.floor(Math.random() * words.length);
+console.log(choosenWord);
+
+//Creates Underscores based off length of Movie Answer//
+
+//Get Users Guess //
+
+//Check if guess is Correct //
+
+// If right, push to right array //
+
+//If wrong, push to wrong array //
