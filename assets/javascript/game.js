bcee92ea5d622636760bda2784328dd238d357dc
Wills Grave <will@keydigitalconsulting.com>
2019-08-19 22:50:56 -0400

    Redid the create array, pick random word and created underscores.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,18 @@
-// Creates an array that lists out all of the options (Alphabet & Movie Answers).
-// var userChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; //
+// Create an array of words //
+var word = ["rudy", "hook", "batman", "snatch", "jaws", "pyscho", "gremlins"];
 
-var word = ['Rudy', 'Jurassic Park', 'Star Wars', 'The Princess Bride', 'Lord of the Rings'];
-
-
-//Picks Movie Answer at Random//
-let randNum = Math.floor(Math.random() * word.length);
-let choosenWord = word[randNum];
-console.log(choosenWord);
-let underScore = [];
+// Pick random word //
+var randNum = Math.floor(Math.random() * word.length);
+var choosenWord = word[randNum];
+var underScore = [];
 console.log(choosenWord);
 
-
-//Creates Underscores based off length of Movie Answer//
-let generateUnderscore = () => {
-    for (var i = 0; i < choosenWord.length; i++) {
-      underScore.push('_');
-    }
-    return underScore;
+// Create underscore based on word length array //
+var generateUnderscore = () => {
+  for(var i = 0; i < choosenWord.length; i++) {
+    underScore.push('_');
+  }
+  return underScore;
 }
 
-console.log(generateUnderscore());
-
-
-//Get Users Guess //
-document.addEventListener('keypress', (event) => {
-  console.log(event);
-});
-
-
-//Check if guess is Correct //
-
-// If right, push to right array //
-
-//If wrong, push to wrong array //
+console.log(generateUnderScore());
