f67a6125258449957eb84af764dee31fca981db5
Wills Grave <will@keydigitalconsulting.com>
2019-08-21 09:07:54 -0400

    Added the remaining steps I need to do for the assignment, pseudocode.

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,3 +1,4 @@
+// Psuedocode all the directions//
 // Press Any Key To Get Started //
 document.onkeyup = function () {
 // Captures user guesses //
@@ -14,4 +15,51 @@ var answer = ["rudy", "hook", "batman", "snatch", "jaws", "pyscho", "gremlins"];
 var rand = answer[Math.floor(Math.random() * answer.length)];
   console.log(rand);
 
+
+// Identify Correct Guesses //
+
+
+
+
+// Identify Incorrect Guesses //
+
+
+
+
+// Make random answers appear with underscores //
+
+
+
+
+// Limit the number of guesses //
+
+
+
+
+
+// Cumulate Wins //
+
+
+
+
+
+// Cumulate Loses //
+
+
+
+
+
+// Restart Game After you win or lose //
+
+
+
+
+
+
+
+
+
+// Bonus //
+// Add Theme song to movie they guess right //
+// Add image from movie when they guess right //
 
